aad22c3815c3e8d46e227f4fa6c31a0dd552858f
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-03 21:01:19 +0000

    yenidne edges

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first3 = word[:3]
 word_last2 = word[-2:]
-word_middle=word[1:-1]
+word_middle = word[1:-1]
 print(f"First 3 letters: {word_first3}")
 print(f"Last 2 letters: {word_last2}")
 print(f"Middle word: {word_middle}")
