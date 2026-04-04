c20bb71566ae9dcc9a76020b6a6bede6f703763d
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-04 08:25:56 +0000

    again again

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
         code = ord(chc)
         if code >= 97 and code <= 122:
             chc = chr(ord(chc) - 32)
-        print({}.format(chc), end="")
+        print("{}".format(chc), end="")
