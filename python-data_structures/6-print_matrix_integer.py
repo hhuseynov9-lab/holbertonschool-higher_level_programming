8e1b90bdf027bbc35ec81910c98019919728f0be
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-08 18:05:47 +0000

    t

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ for i in range(len(row)):
     if i != len(row) - 1:
         print("{:d}".format(row[i]), end=" ")
     else:
-        print("{:d}".format(row[i]), end="") 
+        print("{:d}".format(row[i])) 
