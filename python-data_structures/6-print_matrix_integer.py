cd37421bb46cf76c5f36d4d9a539a876272cd994
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-08 17:22:14 +0000

    wdi

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    row = len(matrix=[[]])
+    row = len(matrix)
     for i in range(len(row)):
         for j in row[i]:
             print("{:d}".format(j))
