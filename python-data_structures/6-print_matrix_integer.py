ee330c35a7d3b910038e1ed3b607e6bcf00905ff
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-08 11:54:32 +0000

    w

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    new = [ [ matrix[i][j]  for j in range(len(matrix[0]))] for i in range(len(matrix[0]))]
+    new = [ ["{:d}".format(imatrix[i][j])  for j in range(len(matrix[0]))] for i in range(len(matrix[0]))]
     return new
