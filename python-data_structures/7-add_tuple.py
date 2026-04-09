c3d6d6e2e1d477e4af428de7b978d8fea640aee9
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-09 06:37:28 +0000

    :w:

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -16,5 +16,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b2 = tuple_b[1]
     else:
         b2 = 0
-    new_tuple = (a1 + a2, b1+ b2)
+    new_tuple = (a1 + b1, b2 + a2)
     return new_tuple
