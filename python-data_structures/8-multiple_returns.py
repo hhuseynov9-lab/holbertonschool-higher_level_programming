cffd35d348d832f6c097afb0e2220af3695b153b
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-09 06:57:57 +0000

    e

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if len(sentence) != 0:
         length = len(sentence)
-        first = sentence[0] 
+        first = sentence[0]
         return (length, first)
     else:
-        return 0,  None
+        return (0, None)
