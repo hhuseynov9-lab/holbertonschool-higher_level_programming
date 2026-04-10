17e3f3a20aa9ba95cd8773d34dc7a92ff3ab3852
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-10 21:17:46 +0000

    e

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) != 0:
-        max1 =  max(a_dictionary.values())
-        for i in a_dictionary:
-            if a_dictionary[i] == max1:
-                print("{}".format(i))
-
-    else:
+    # Əgər lüğət boşdursa və ya None-dırsa
+    if not a_dictionary:
         return None
+    
+    # Ən yüksək balı tapırıq
+    max_val = max(a_dictionary.values())
+    
+    # Həmin bala uyğun olan İLK açarı qaytarırıq
+    for i in a_dictionary:
+        if a_dictionary[i] == max_val:
+            return i  # Tapdıq və dərhal funksiyanı bitirib nəticəni qaytarırıq
