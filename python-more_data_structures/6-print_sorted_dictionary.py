5be589feb106ca2011666189c363b9411ff16215
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-10 19:35:59 +0000

    f

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return sorted(a_dictionary)
+    new = a_dictionary
+    for i in new:
+        print("{} : {}".format(i, new[i]))
