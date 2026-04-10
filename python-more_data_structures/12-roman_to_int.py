4f4afcbb62b8cd352dc0b0d2c07b5d63630f126a
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-10 21:54:01 +0000

    e

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
     c = []
     cem = 0
     roman_values = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000}
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
     for s in roman_string:
         for key, value in roman_values.items():
             if s == key:
                 c.append(value)
-    for i in range(len(c)-1):
-        if c[i] > c[i+1]:
+    if len(c) == 0:
+        return 0
+    for i in range(len(c) - 1):
+        if c[i] >= c[i+1]:
             cem = cem + c[i]
         else:
             cem = cem - c[i]
+    cem = cem + c[-1]
 
-    cem = cem + c[i+1]
     return cem
