0cc11663de8f9cfe93a8a9ce5426ebaed895d4e1
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-10 20:12:43 +0000

    dede

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     new = sorted(a_dictionary)
     for i in new:
-        print("{}: {}".format(i, new[i]))
+        print("{}: {}".format(i, a_dictionary[i]))
