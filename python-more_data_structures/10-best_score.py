c0895d719112095ced3cde7477e616e1cca413af
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-10 21:22:15 +0000

    f

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    # Əgər lüğət boşdursa və ya None-dırsa
+    """Returns a key with the biggest integer value."""
     if not a_dictionary:
         return None
-    # Ən yüksək balı tapırıq
-    max_val = max(a_dictionary.values())   
-    # Həmin bala uyğun olan İLK açarı qaytarırıq
-    for i in a_dictionary:
-        if a_dictionary[i] == max_val:
-            return i  # Tapdıq və dərhal funksiyanı bitirib nəticəni qaytarırıq
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return max_key
