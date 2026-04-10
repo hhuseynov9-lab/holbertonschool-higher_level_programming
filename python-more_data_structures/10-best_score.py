e56ab78cfaa868827e87669b4ddaa94b9a265a69
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-10 21:13:44 +0000

    e

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if len(a_dictionary) != 0:
-        return max(a_dictionary.values())
+        max1 =  max(a_dictionary.values())
+        for i in a_dictionary:
+            if a_dictionary[i] == max1:
+                print("{}".format(i))
+
     else:
         return None
