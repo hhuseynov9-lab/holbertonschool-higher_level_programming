6451f83c1da066f7c43b6226e777e8081f3b6cf6
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-11 19:04:02 +0000

    w

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,13 +3,22 @@
 ff
 '''
 class Square:
+    '''
+    f
+    '''
 
     def __init__(self, size=0):
+       '''
+       f
+       '''
         if type(size)  is not int:
             raise TypeError("size must be integer")
         if size < 0:
             raise ValueError("size must be >= 0")
     def area(self):
+       '''
+       f
+       '''
         self.size = size * size
     
     self.__size
