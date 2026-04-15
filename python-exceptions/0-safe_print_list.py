8d1fd84061ef6168a4a336a531bf727b19ed601d
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-15 18:56:58 +0000

    w

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-   for i in range(x):
-       try:
-           print("{}".format(my_list[i]), end="")
+    printed_count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed_count += 1
         except IndexError:
-            print("birde olmasin")
             break
+    print("") 
+    return printed_count
