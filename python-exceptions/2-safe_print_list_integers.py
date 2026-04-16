70c84299c9c74e72bf93a203aa1c160a5dad50f5
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-16 06:56:20 +0000

    w

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,12 @@ def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     for i in range(x):
         try:
-            counter += 1
             print("{:d}".format(my_list[i]), end="")
-        except (IndexError, ValueError):
+            counter += 1
+        except IndexError:
+            break
+        except ValueError:
             continue
-    print("{:d}".format(counter))
+
+    print("")
+    return counter
