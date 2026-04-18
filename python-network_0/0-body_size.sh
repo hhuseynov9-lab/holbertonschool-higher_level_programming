fee3528a9741f329c493660e183328a46994a731
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-18 17:55:14 +0000

    task team

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# Bu skript daxil edilən URL-in body ölçüsünü baytla göstərir
-
-# 1. İstifadəçinin bir URL daxil edib-etmədiyini yoxlayırıq
-if [ -z "$1" ]; then
-    echo "İstifadə qaydası: ./script.sh <URL>"
-    exit 1
-fi
-
-# 2. curl əmrini işlədirik
-# -s: Proqres barı gizlədir (artıq bilirsən!)
-# -o /dev/null: Cavabın özünü (məs. HTML-i) ekrana çıxarmır, "zibil qutusuna" atır
-# -w: Spesifik məlumatları (metadata) çıxarmaq üçün işlədilir
-curl -s "$1" -o /dev/null -w "%{size_download}\n"
+# Displays the size of the body of a response in bytes
+curl -s "$1" | wc -c
