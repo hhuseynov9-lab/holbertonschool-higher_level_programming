abc7cd8fbdea19d4c4a454a54123f87594f929d1
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-18 05:10:54 +0000

    e

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Creates a table second_table with columns id, name and score
+-- Also inserts multiple rows into the table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES 
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
