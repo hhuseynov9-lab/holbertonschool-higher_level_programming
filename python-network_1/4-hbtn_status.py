60cc3421ddc087957ebd1efff786b7955b0700db
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-23 21:18:07 +0000

    update

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,7 +8,7 @@ import requests
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     r = requests.get(url)
-    
+
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
