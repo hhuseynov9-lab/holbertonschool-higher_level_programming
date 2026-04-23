2668536b04660aedd1d89e4ea1c6b2d592e09507
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-23 21:28:14 +0000

    update

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -10,6 +10,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
-    x-Request-Id = r.headers.get('X-Request-Id')
+    xcRequestcId = r.headers.get('X-Request-Id')
     if xcRequestcId:
         print(xcRequestcId)
