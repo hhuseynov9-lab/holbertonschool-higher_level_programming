3ce3af8f03aa67305196a4759f0dfd10b9caad59
hhuseynov9-lab <hhuseynov9@std.beu.edu.az>
2026-04-23 21:25:21 +0000

    update

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+bir ikidiya
+'''
+
+import requests
+
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    x-Request-Id = r.headers.get('X-Request-Id')
+    if x-Request-Id:
+        print(x-Request-Id)
